12360be45d9108d0749e9731fe66a889d69ba0e8
kakl <kakl@astro.cz>
2024-04-26 16:20:48 +0200

    skeleton

---
+++ fw/tank/tank.ino
@@ -0,0 +1,131 @@
+
+/*
+  TANK  
+
+ISP
+---
+PD0     RX
+PD1     TX
+RESET#  through 50M capacitor to RST#
+
+SDcard
+------
+DAT3   SS   4 B4
+CMD    MOSI 5 B5
+DAT0   MISO 6 B6
+CLK    SCK  7 B7
+
+ANALOG
+------
++      A0  PA0
+-      A1  PA1
+RESET  0   PB0
+
+LED
+---
+LED_yellow  23  PC7         // LED for Dasa
+
+
+                     Mighty 1284p    
+                      +---\/---+
+           (D 0) PB0 1|        |40 PA0 (AI 0 / D24)
+           (D 1) PB1 2|        |39 PA1 (AI 1 / D25)
+      INT2 (D 2) PB2 3|        |38 PA2 (AI 2 / D26)
+       PWM (D 3) PB3 4|        |37 PA3 (AI 3 / D27)
+    PWM/SS (D 4) PB4 5|        |36 PA4 (AI 4 / D28)
+      MOSI (D 5) PB5 6|        |35 PA5 (AI 5 / D29)
+  PWM/MISO (D 6) PB6 7|        |34 PA6 (AI 6 / D30)
+   PWM/SCK (D 7) PB7 8|        |33 PA7 (AI 7 / D31)
+                 RST 9|        |32 AREF
+                VCC 10|        |31 GND
+                GND 11|        |30 AVCC
+              XTAL2 12|        |29 PC7 (D 23)
+              XTAL1 13|        |28 PC6 (D 22)
+      RX0 (D 8) PD0 14|        |27 PC5 (D 21) TDI
+      TX0 (D 9) PD1 15|        |26 PC4 (D 20) TDO
+RX1/INT0 (D 10) PD2 16|        |25 PC3 (D 19) TMS
+TX1/INT1 (D 11) PD3 17|        |24 PC2 (D 18) TCK
+     PWM (D 12) PD4 18|        |23 PC1 (D 17) SDA
+     PWM (D 13) PD5 19|        |22 PC0 (D 16) SCL
+     PWM (D 14) PD6 20|        |21 PD7 (D 15) PWM
+                      +--------+
+*/
+
+/*
+// Compiled with: Arduino 1.8.9
+// MightyCore 2.0.2 https://mcudude.github.io/MightyCore/package_MCUdude_MightyCore_index.json
+Fix old bug in Mighty SD library
+~/.arduino15/packages/MightyCore/hardware/avr/2.0.2/libraries/SD/src/SD.cpp:
+boolean SDClass::begin(uint32_t clock, uint8_t csPin) {
+  if(root.isOpen()) root.close();
+*/
+
+#define LED         23   // PC7
+#define RESET       0    // PB0
+#define SDpower1    1    // PB1
+#define SDpower2    2    // PB2
+#define SDpower3    3    // PB3
+#define SS          4    // PB4
+#define MOSI        5    // PB5
+#define MISO        6    // PB6
+#define SCK         7    // PB7
+#define INT         20   // PC4
+
+
+
+// Read Analog Differential without gain (read datashet of ATMega1280 and ATMega2560 for refference)
+// Use analogReadDiff(NUM)
+//   NUM  | POS PIN             | NEG PIN           |   GAIN
+//  0 | A0      | A1      | 1x
+//  1 | A1      | A1      | 1x
+//  2 | A2      | A1      | 1x
+//  3 | A3      | A1      | 1x
+//  4 | A4      | A1      | 1x
+//  5 | A5      | A1      | 1x
+//  6 | A6      | A1      | 1x
+//  7 | A7      | A1      | 1x
+//  8 | A8      | A9      | 1x
+//  9 | A9      | A9      | 1x
+//  10  | A10     | A9      | 1x
+//  11  | A11     | A9      | 1x
+//  12  | A12     | A9      | 1x
+//  13  | A13     | A9      | 1x
+//  14  | A14     | A9      | 1x
+//  15  | A15     | A9      | 1x
+#define PIN 0
+uint8_t analog_reference = INTERNAL2V56; // DEFAULT, INTERNAL, INTERNAL1V1, INTERNAL2V56, or EXTERNAL
+
+void setup()
+{
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+
+  Serial.println("#Cvak...");
+  
+ 
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);  
+  
+  //!!! Wire.setClock(100000);
+
+  for(int i=0; i<5; i++)  
+  {
+    delay(50);
+    digitalWrite(LED, HIGH);  // Blink for Dasa 
+    delay(50);
+    digitalWrite(LED, LOW);  
+  }
+
+  Serial.println("#Hmmm...");
+
+}
+
+
+void loop()
+{
+    delay(500);
+    digitalWrite(LED, HIGH);  // Blink for Dasa 
+    delay(500);
+    digitalWrite(LED, LOW);  
+}
